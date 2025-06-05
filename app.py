ec9ae3622f3d3373a69cd30550bb79bad265e78e
Gökay Şahin <katex35@hotmail.com>
2025-06-05 14:56:26 +0300

    cors update

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {
+        "origins": ["http://127.0.0.1:5500", "http://localhost:5500"],
+        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        "allow_headers": ["Content-Type"]
+    }
+})
 
 @app.route('/')
 def home():
